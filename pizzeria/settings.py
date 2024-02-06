6d5d03763d5d1c35488dd3b19a869c3f65075f8c
Michael-Tyson <156892756+Michael-Tyson@users.noreply.github.com>
2024-02-06 20:17:35 +0500

    Update settings.py

--- pizzeria/settings.py
+++ pizzeria/settings.py
@@ -90,9 +90,24 @@ WSGI_APPLICATION = 'pizzeria.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': '',
+#         'USER': '',
+#         'PASSWORD': '',
+#         'HOST': '',
+#         'PORT': '5432'
+#     }
+# }
 
-
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
 
 
 # Password validation
@@ -149,7 +164,7 @@ import dj_database_url
 
 #heroku settings
 DATABASES ={
-    'default': dj_database_url.config(default='postgres://localhost')
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
         }
     #Honor the 'X-fowarded-proto' header for request.is_secure().
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO','http')

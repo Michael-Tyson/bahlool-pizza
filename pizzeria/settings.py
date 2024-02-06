976d74be1adb62d9656fb804d25294708eb711ee
Michael-Tyson <156892756+Michael-Tyson@users.noreply.github.com>
2024-02-06 19:59:47 +0500

    Update settings.py

--- pizzeria/settings.py
+++ pizzeria/settings.py
@@ -91,11 +91,7 @@ WSGI_APPLICATION = 'pizzeria.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+
 }
 
 
